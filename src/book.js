ae693fdb128ba955a0c3091c24430d0f61cccdc5
Amber Shipley <ashipley2@me.com>
2022-12-02 09:03:45 -0800

    Add working functions

--- src/book.js
+++ src/book.js
@@ -1,12 +1,36 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {name, age, pronouns}
+}
+
+function saveReview(review, reviews) {
+  if (reviews.includes(review) || reviews.includes(review)) {
+    return reviews
+  } else {
+    return reviews.push(review)
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  return 20 * bookTitle.length
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  return {title: bookTitle, mainCharacter: bookCharacter, pageCount: calculatePageCount(bookTitle), genre: genre}
+}
+
+function editBook(book) {
+ book.pageCount *=  0.75
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
