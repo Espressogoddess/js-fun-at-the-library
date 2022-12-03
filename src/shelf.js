d385e12204fdac8c3ceec7c5b17fb5f0955cc1e7
Amber Shipley <ashipley2@me.com>
2022-12-03 15:09:14 -0800

    Create functions to pass tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,38 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book)
+  }
+}
+
+function unshelfBook(book, shelf) {
+  var position
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === book) {
+       position = i
+    } 
+  }
+  shelf.splice(position, 1)
+}
+
+function listTitles(shelf) {
+  var titles = []
+  for (var i = 0; i < shelf.length; i++) {
+   titles.push(shelf[i].title)
+  } return titles.join(', ')
+}
+
+function searchShelf(shelf, book) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === book) {
+      return true
+    }
+  }
+  return false
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
