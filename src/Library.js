c7ea657a489954925d5d5f4477ce65f79e368658
Amber Shipley <ashipley2@me.com>
2022-12-03 18:28:49 -0800

    Implement createLibrary and addBook functions
    Also uncomment tests, checkoutBook WIP

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,31 @@
+function createLibrary(libraryName) {
+  return {
+    name: libraryName,
+    shelves: {fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+
+}
+
+function addBook(library, bookName) {
+  if (bookName.genre === 'fantasy') {
+    library.shelves.fantasy.push(bookName)
+  } else {
+    library.shelves.nonFiction.push(bookName)
+  }
+}
+
+function checkoutBook(library, bookName, genre) {
+
+
+
+}
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
