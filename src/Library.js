0e20b885d3164836d11aed3ead90f4e00be0a4d2
Amber Shipley <ashipley2@me.com>
2022-12-04 16:36:51 -0800

    Complete library functions

--- src/Library.js
+++ src/Library.js
@@ -1,26 +1,28 @@
 function createLibrary(libraryName) {
   return {
     name: libraryName,
-    shelves: {fantasy: [],
+    shelves: {
+      fantasy: [],
       fiction: [],
       nonFiction: []
     }
   }
-
 }
 
-function addBook(library, bookName) {
-  if (bookName.genre === 'fantasy') {
-    library.shelves.fantasy.push(bookName)
-  } else {
-    library.shelves.nonFiction.push(bookName)
-  }
+function addBook(library, book) {
+  library.shelves[book.genre].push(book)
 }
+
 
 function checkoutBook(library, bookName, genre) {
-
-
-
+  var genreBooksArray = library.shelves[genre]
+  for (var i = 0; i < genreBooksArray.length; i++) {
+    if (genreBooksArray[i].title === bookName) {
+      genreBooksArray.splice(i, 1)
+      return `You have now checked out ${bookName} from the ${library.name}`
+    } 
+  } 
+  return `Sorry, there are currently no copies of ${bookName} available at the ${library.name}`
 }
 
 
